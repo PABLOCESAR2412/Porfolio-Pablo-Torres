90041ad3ee51a4995523ee630468a9eab229b88c
Pablo Cesar Torres GIlse <pablocesar.t.g@gmail.com>
2026-07-05 00:00:22 -0500

    chore: Apply global transition sync and enable Astro prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,8 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'hover',
+  },
 });
